a6c457819c451b9210c561d2ea60f2181d0efad0
Katie Davidson <katie.davidson92@gmail.com>
2024-05-03 12:06:21 -0700

    updated compilation with new EPRO output format

--- scripts/misc-helpers/EPROcompile.R
+++ scripts/misc-helpers/EPROcompile.R
@@ -20,49 +20,50 @@ library(writexl)
 # Read OtoManager files as large list ---------------------------
 # Load base files to compile
 wcviCNepro.LL <- lapply(list.files("//dcbcpbsna01a.ENT.dfo-mpo.ca/SCD_Stad/WCVI/CHINOOK/WCVI_TERMINAL_RUN/Annual_data_summaries_for_RunRecons/EPROcompile_base-files/1-Import-to-R", 
-                                 pattern=".xlsx", full.names=T), 
+                                 pattern=".csv", full.names=T), 
                       function(x) {
-                        readxl::read_excel(x, sheet=1, guess_max=20000)
+                        read.csv(x)
                       })
 
 # Change filenames in the List:
 names(wcviCNepro.LL) <- list.files("//dcbcpbsna01a.ENT.dfo-mpo.ca/SCD_Stad/WCVI/CHINOOK/WCVI_TERMINAL_RUN/Annual_data_summaries_for_RunRecons/EPROcompile_base-files/1-Import-to-R", 
-                                 pattern=".xlsx", full.names=F)
+                                 pattern=".csv", full.names=F)
 
 
 # Convert the Large List into a useable R dataframe ---------------------------
 wcviCNepro <- do.call("rbind", wcviCNepro.LL) %>%
   tibble::rownames_to_column(var="file_source") %>%
-  filter(`Spawning Stock` != "") %>%
+  filter(Spawning.Stock != "") %>%
   mutate(
     #across(everything(), parse_guess), # Automatically determine column classes based on values
-    `(R) RETURN YEAR` = as.numeric(substr(`Spawning Stock`, 1,4)),
-    `(R) OTOLITH BOX NUM` = `Bag No`,
-    `(R) OTOLITH VIAL NUM` = `Vial No`,
-    `(R) OTOLITH BOX-VIAL CONCAT` = case_when(!is.na(`Bag No`) & !is.na(`Vial No`) ~ paste0(`Bag No`,sep="-",`Vial No`)),
-    `(R) SCALE BOOK NUM` = `Book No`,
-    `(R) SCALE CELL NUM` = `Scale Sample No`,
-    `(R) SCALE BOOK-CELL CONCAT` = case_when(!is.na(`Book No`) & !is.na(`Scale Sample No`) ~ paste0(`Book No`,sep="-",`Scale Sample No`)),
-    `(R) DNA NUM` = `DNA Sample No`,
-    `(R) TAGCODE` = `CWT Tag Code`,
-    `(R) HATCHCODE` = `Hatch Code`,
-    `(R) TOTAL AGE - CWT` = case_when(!is.na(`CWT Age (yrs)`) ~ as.numeric(`CWT Age (yrs)`),
+    `(R) RETURN YEAR` = as.numeric(substr(Spawning.Stock, 1,4)),
+    `(R) OTOLITH BOX NUM` = Bag.No,
+    `(R) OTOLITH VIAL NUM` = Vial.No,
+    `(R) OTOLITH BOX-VIAL CONCAT` = case_when(!is.na(Bag.No) & !is.na(Vial.No) ~ paste0(Bag.No,sep="-",Vial.No)),
+    `(R) SCALE BOOK NUM` = Book.No,
+    `(R) SCALE CELL NUM` = Scale.Sample.No,
+    `(R) SCALE BOOK-CELL CONCAT` = case_when(!is.na(Book.No) & !is.na(Scale.Sample.No) ~ paste0(Book.No,sep="-",Scale.Sample.No)),
+    `(R) DNA NUM` = DNA.Sample.No,
+    `(R) TAGCODE` = CWT.Tag.Code,
+    `(R) HATCHCODE` = Hatch.Code,
+    `(R) TOTAL AGE: CWT` = case_when(!is.na(CWT.Age..yrs.) ~ as.numeric(CWT.Age..yrs.),
                                                TRUE ~ NA),
-    `(R) TOTAL AGE - SCALE` = case_when(!is.na(`Scale Total Age (yrs)`) ~ as.numeric(`Scale Total Age (yrs)`), # Some entries for ttl age are "TRUE"(??)
-                                                 `Scale Part Age`=="1M" ~ 2,
-                                                 `Scale Part Age`=="2M" ~ 3,
-                                                 `Scale Part Age`=="3M" ~ 4,
-                                                 `Scale Part Age`=="4M" ~ 5,
-                                                 `Scale Part Age`=="5M" ~ 6,
-                                                 `Scale Part Age`=="6M" ~ 7,
+    `(R) TOTAL AGE: SCALE` = case_when(!is.na(Scale.Total.Age..yrs.) ~ as.numeric(Scale.Total.Age..yrs.), # Some entries for ttl age are "TRUE"(??)
+                                                 Scale.Part.Age=="1M" ~ 2,
+                                                 Scale.Part.Age=="2M" ~ 3,
+                                                 Scale.Part.Age=="3M" ~ 4,
+                                                 Scale.Part.Age=="4M" ~ 5,
+                                                 Scale.Part.Age=="5M" ~ 6,
+                                                 Scale.Part.Age=="6M" ~ 7,
                                                  T ~ NA_real_),
-    `(R) BROOD YEAR CWT` = `(R) RETURN YEAR` - `(R) TOTAL AGE - CWT`,
-    `(R) BROOD YEAR SCALE` = `(R) RETURN YEAR` - `(R) TOTAL AGE - SCALE`,
-    `(R) RESOLVED BROOD YEAR` = case_when(!is.na(`(R) BROOD YEAR CWT`) ~ `(R) BROOD YEAR CWT`,
-                                          is.na(`(R) BROOD YEAR CWT`) ~ `(R) TOTAL AGE - SCALE`,
+    `(R) BROOD YEAR: CWT` = `(R) RETURN YEAR` - `(R) TOTAL AGE: CWT`,
+    `(R) BROOD YEAR: SCALE` = `(R) RETURN YEAR` - `(R) TOTAL AGE: SCALE`,
+    `(R) RESOLVED BROOD YEAR` = case_when(!is.na(`(R) BROOD YEAR: CWT`) ~ `(R) BROOD YEAR: CWT`,
+                                          is.na(`(R) BROOD YEAR: CWT`) ~ `(R) BROOD YEAR: SCALE`,
                                           TRUE ~ NA)
     #UEID = paste0(analysis_year, "-", seq(1:nrow(.)))
     ) %>%
+  filter(grepl("Chinook", Spawning.Stock)) %>%
   print()
 
 # Clean up ---------------------------

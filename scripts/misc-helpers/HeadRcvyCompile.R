529a3ba139adcadd7111f04cd71a33a6b1a6e12f
Katie Davidson <katie.davidson92@gmail.com>
2024-06-27 12:30:57 -0700

    update folder names

--- scripts/misc-helpers/HeadRcvyCompile.R
+++ scripts/misc-helpers/HeadRcvyCompile.R
@@ -20,14 +20,14 @@
 
 # Read CWT recovery files as large list ---------------------------
 # Load base files to compile
-mrpHeadRcvy.LL <- lapply(list.files("//dcbcpbsna01a.ENT.dfo-mpo.ca/SCD_Stad/WCVI/CHINOOK/WCVI_TERMINAL_RUN/Annual_data_summaries_for_RunRecons/HeadRcvyCompile_base-files/Import", 
+mrpHeadRcvy.LL <- lapply(list.files("//dcbcpbsna01a.ENT.dfo-mpo.ca/SCD_Stad/WCVI/CHINOOK/WCVI_TERMINAL_RUN/Annual_data_summaries_for_RunRecons/HeadRcvyCompile_base-files/1-Import-to-R", 
                                     pattern=".csv", full.names=T), 
                          function(x) {
                            read.csv(x)
                          })
 
 # Change filenames in the List:
-names(mrpHeadRcvy.LL) <- list.files("//dcbcpbsna01a.ENT.dfo-mpo.ca/SCD_Stad/WCVI/CHINOOK/WCVI_TERMINAL_RUN/Annual_data_summaries_for_RunRecons/HeadRcvyCompile_base-files/Import", 
+names(mrpHeadRcvy.LL) <- list.files("//dcbcpbsna01a.ENT.dfo-mpo.ca/SCD_Stad/WCVI/CHINOOK/WCVI_TERMINAL_RUN/Annual_data_summaries_for_RunRecons/HeadRcvyCompile_base-files/1-Import-to-R", 
                                  pattern=".csv", full.names=T)
 
 

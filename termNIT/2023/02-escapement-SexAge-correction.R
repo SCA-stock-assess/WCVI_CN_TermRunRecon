5a2cd11a8f3b4a5ae1d762c078b9f8846473ccd1
Katie Davidson <katie.davidson92@gmail.com>
2024-08-30 13:44:18 -0700

    added summarize and join, and export to mapping file

--- termNIT/2023/02-escapement-SexAge-correction.R
+++ termNIT/2023/02-escapement-SexAge-correction.R
@@ -15,6 +15,7 @@ fecundity_at_age <- tibble(`(R) RESOLVED TOTAL AGE` = c(2:6),
 "%notin%" <- Negate("%in%")
 options(scipen=9999)
 
+analysis_year <- 2023
 
 
 # ============================= LOAD DATA =============================
@@ -47,34 +48,38 @@ NITepro <- readxl::read_excel(path=paste0("//dcbcpbsna01a.ENT.dfo-mpo.ca/SCD_Sta
 
 sexAgeCorrection <- full_join(
   # ---- Age proportions from SAMPLE (close to 50:50): 
-  sexAgeCorrection_brokenOut <- full_join(NITepro %>% 
-              filter(`(R) RETURN YEAR` %in% NITmap$TermRun_Year & grepl("Nitinat R Fall Chinook", Spawning.Stock) & `(R) RESOLVED TOTAL AGE`%in%c(2:6)) %>%
-              group_by(Maturity.Class, `(R) RESOLVED TOTAL AGE`) %>% 
-              summarize(n_age=n()) %>% 
-              group_by(Maturity.Class) %>% 
-              mutate(n_sample=sum(n_age),
-                     propn_age_sample = n_age/n_sample) %>% 
-              ungroup() %>% 
-              group_by(`(R) RESOLVED TOTAL AGE`) %>% 
-              ## (new) JACK CORRECTION NUMERICAL: 
-              mutate(n_age_jackCORR = case_when(Maturity.Class=="Female" ~ n_age,
-                                                 Maturity.Class=="Jack" & `(R) RESOLVED TOTAL AGE`!=2 ~ n_age-n_age,
-                                                 Maturity.Class=="Male" & `(R) RESOLVED TOTAL AGE`==2 ~ n_age-n_age)) %>% 
-              group_by(`(R) RESOLVED TOTAL AGE`, Maturity.Class%in%c("Male", "Jack")) %>% 
-              mutate(n_age_jackCORR = case_when(is.na(n_age_jackCORR) ~ sum(n_age),
-                                                 TRUE~n_age_jackCORR)) %>%
-              ungroup() %>%
-              select(-c(`Maturity.Class %in% c("Male", "Jack")`)) %>% 
-              group_by(Maturity.Class) %>% 
-              # ---- CORRECTED age proportions (for jack ID issues): 
-              mutate(n_sample_jackCORR = sum(n_age_jackCORR),
-                     propn_age_sample_jackCORR = n_age_jackCORR/unique(n_sample_jackCORR)) %>% 
-              ungroup(),
-            full_age_range) %>% 
+  sexAgeCorrection_brokenOut <- full_join(
+    NITepro %>% 
+      filter(`(R) RETURN YEAR` %in% NITmap$TermRun_Year &     ## change this line if you want to do a multi-year average (would need to add extra code for this)
+               grepl("Nitinat R Fall Chinook", Spawning.Stock) & 
+               `(R) RESOLVED TOTAL AGE`%in%c(2:6)) %>%
+      group_by(`(R) RETURN YEAR`, Maturity.Class, `(R) RESOLVED TOTAL AGE`) %>% 
+      summarize(n_age=n()) %>% 
+      group_by(Maturity.Class) %>% 
+      mutate(n_sample=sum(n_age),
+             propn_age_sample = n_age/n_sample) %>% 
+      ungroup() %>% 
+      group_by(`(R) RESOLVED TOTAL AGE`) %>% 
+      ## (new) JACK CORRECTION NUMERICAL: 
+      mutate(n_age_jackCORR = case_when(Maturity.Class=="Female" ~ n_age,
+                                        Maturity.Class=="Jack" & `(R) RESOLVED TOTAL AGE`!=2 ~ n_age-n_age,
+                                        Maturity.Class=="Male" & `(R) RESOLVED TOTAL AGE`==2 ~ n_age-n_age)) %>% 
+      group_by(`(R) RESOLVED TOTAL AGE`, Maturity.Class%in%c("Male", "Jack")) %>% 
+      mutate(n_age_jackCORR = case_when(is.na(n_age_jackCORR) ~ sum(n_age),
+                                        TRUE~n_age_jackCORR)) %>%
+      ungroup() %>%
+      select(-c(`Maturity.Class %in% c("Male", "Jack")`)) %>% 
+      group_by(Maturity.Class) %>% 
+      # ---- CORRECTED age proportions (for jack ID issues): 
+      mutate(n_sample_jackCORR = sum(n_age_jackCORR),
+             propn_age_sample_jackCORR = n_age_jackCORR/unique(n_sample_jackCORR)) %>% 
+      ungroup() %>%
+      rename(TermRun_AGES_year = `(R) RETURN YEAR`),
+    full_age_range) %>% 
   complete(Maturity.Class, `(R) RESOLVED TOTAL AGE`, fill=list(`(R) RESOLVED TOTAL AGE`=0), explicit=F) %>%
   mutate(across(c(n_age, propn_age_sample, n_age_jackCORR, propn_age_sample_jackCORR), ~case_when(is.na(.)~0, TRUE~.))) %>%
   group_by(Maturity.Class) %>%
-  fill(n_sample, .direction="updown") %>% 
+  fill(c(TermRun_AGES_year, n_sample), .direction="updown") %>% 
   # (Import escapement estimate and observed sex ratio from mapping file to do the rest of the math): 
   mutate(escapement_estimate = NITmap[NITmap$TermRun_sector01=="Escapement - mainstem" & NITmap$TermRun_sex_strata=="Total (incl Jacks)",]$Enumeration,
          true_sex_ratio = case_when(Maturity.Class=="Male" ~ NITmap[NITmap$TermRun_sector02=="Actual sex ratio (from hatchery staff)" & 
@@ -92,11 +97,12 @@ sexAgeCorrection <- full_join(
   full_join(.,
             NITepro %>%
               filter(`(R) RETURN YEAR` %in% NITmap$TermRun_Year & grepl("Nitinat R Fall Chinook", Spawning.Stock) & `(R) RESOLVED TOTAL AGE`%in%c(2:6)) %>%
-              group_by(`(R) RESOLVED TOTAL AGE`) %>%
+              group_by(`(R) RETURN YEAR`, `(R) RESOLVED TOTAL AGE`) %>%
               summarize(n_age = n()) %>%
               mutate(propn_age_sample = n_age/sum(n_age),
                      Maturity.Class="Total",
-                     n_sample=sum(n_age))) %>%
+                     n_sample=sum(n_age)) %>%
+              rename(TermRun_AGES_year = `(R) RETURN YEAR`)) %>%
   select(-c(n_age)) %>%
   group_by(`(R) RESOLVED TOTAL AGE`) %>% 
   mutate(propn_sexAge_CORR = case_when(Maturity.Class=="Total" ~ sum(propn_sexAge_CORR,na.rm=T)/1,
@@ -114,7 +120,7 @@ sexAgeCorrection <- full_join(
 sexAgeCorrection_brokenOut %>% 
   mutate(Maturity.Class = case_when(Maturity.Class %in% c("Male", "Jack") ~ "Males (incl Jacks)",
                                            TRUE ~ Maturity.Class)) %>%
-  group_by(Maturity.Class, `(R) RESOLVED TOTAL AGE`, escapement_estimate) %>% 
+  group_by(TermRun_AGES_year, Maturity.Class, `(R) RESOLVED TOTAL AGE`, escapement_estimate) %>% 
   summarize(n_sample = sum(n_sample),
             n_sexAge_CORR = sum(n_sexAge_CORR)) %>%
   group_by(Maturity.Class) %>% 
@@ -125,14 +131,17 @@ sexAgeCorrection_brokenOut %>%
                                               TRUE ~ propn_sexAge_CORR),
          TermRun_AGEStemp = "Broodstock corrected",
          TermRun_AGESspat = "Broodstock corrected",
-         TermRun_AGESsex = paste0("Broodstock corrected - ", Maturity.Class),
-  ) 
+         TermRun_AGESsex = paste0("Broodstock corrected - ", Maturity.Class)) 
 ) %>% 
   # 3. Fecundity/egg deposition ---------------------------------------------
+  ungroup() %>%  
   left_join(.,
             fecundity_at_age) %>% 
   mutate(total_fecundity = n_sexAge_CORR*fecundity,
-         total_egg_deposition = sum(total_fecundity, na.rm=T)) %>%
+         total_egg_deposition = case_when(Maturity.Class=="Female" ~ sum(total_fecundity, na.rm=T),
+                                          TRUE ~ NA),
+         propn_sexAge_CORR = case_when(Maturity.Class=="Jack" & `(R) RESOLVED TOTAL AGE`==2 ~ propn_age_sample,
+                                       TRUE ~ propn_sexAge_CORR)) %>%
   print()
 
 
@@ -147,18 +156,18 @@ remove(sexAgeCorrection_brokenOut)
 writexl::write_xlsx(sexAgeCorrection, 
                     path=paste0(here::here("outputs"), 
                                 "/R_OUT - Nitinat sex correction ",
-                                max(NITmap$TermRun_Year),
+                                max(sexAgeCorrection$TermRun_AGES_year),
                                 ".xlsx"))
 
 
 
 # To DFO Network drive ---------------------------
-openxlsx::saveWorkbook(sexAgeCorrection, 
-                       path=paste0("//dcbcpbsna01a.ENT.dfo-mpo.ca/SCD_Stad/WCVI/CHINOOK/WCVI_TERMINAL_RUN/TERMNIT/Nit",
-                                   max(NITmap$TermRun_Year),
-                                   "/R_OUT - Nitinat sex correction ",
-                                   max(NITmap$TermRun_Year),
-                                   ".xlsx"))
+writexl::write_xlsx(sexAgeCorrection, 
+                    path=paste0("//dcbcpbsna01a.ENT.dfo-mpo.ca/SCD_Stad/WCVI/CHINOOK/WCVI_TERMINAL_RUN/TERMNIT/Nit",
+                                max(sexAgeCorrection$TermRun_AGES_year),
+                                "/R_OUT - Nitinat sex correction ",
+                                max(sexAgeCorrection$TermRun_AGES_year),
+                                ".xlsx"))
 
 
 
@@ -168,12 +177,22 @@ openxlsx::saveWorkbook(sexAgeCorrection,
 #                                                                     JOIN to mapping file
 
 
-# Join a simplified output 
-tt <- left_join(NITmap,
-                NITage_summary,
-                by=c("TermRun_AGEStemp", "TermRun_AGESspat", "TermRun_AGESsex", "TermRun_AGES_year"))
+# ============================== JOIN simplified output to mapping file ==============================
+NITmap02 <- left_join(NITmap,
+                      sexAgeCorrection %>% 
+                        select(TermRun_AGES_year, Maturity.Class, `(R) RESOLVED TOTAL AGE`, propn_sexAge_CORR:TermRun_AGESsex) %>% 
+                        pivot_wider(names_from=`(R) RESOLVED TOTAL AGE`, names_prefix="propn_age_", values_from=propn_sexAge_CORR),
+                      by=c("TermRun_AGEStemp", "TermRun_AGESspat", "TermRun_AGESsex", "TermRun_AGES_year"))
 
 
+# ============================== EXPORT ==============================
+# To github repo ---------------------------
+writexl::write_xlsx(NITmap02, 
+                    path=paste0(here::here("termNIT"), "/", analysis_year, 
+                                "/TERMNIT_mapping_",
+                                analysis_year,
+                                "-02",
+                                ".xlsx"))
 
 
 

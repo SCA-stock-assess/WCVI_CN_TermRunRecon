fb8fe4bbdd89b2bfcd665a114213a5d9a3b437df
Katie Davidson <katie.davidson92@gmail.com>
2024-10-07 13:12:25 -0700

    new regex for list.files; minor updates to existing age calcs; added join and export using coalesce - chunky for now may need cleaner option later

--- termNIT/2023/03-escapement.R
+++ termNIT/2023/03-escapement.R
@@ -23,7 +23,7 @@ analysis_year <- 2023
 # Read mapping file -------------------------------
 NITmap02 <- readxl::read_excel(path=paste0(here::here("termNIT"), "/", analysis_year, "/", 
                                            list.files(path=paste0(here::here("termNIT"), "/", analysis_year),
-                                                                  pattern="R_OUT - TERMNIT_mapping_[0-9]{4}-output_from_02\\.xlsx$",
+                                                                  pattern="^R_OUT - TERMNIT_mapping_[0-9]{4}-output_from_02\\.xlsx$",
                                                       full.names=F)),   
                              sheet="Sheet1")
 
@@ -43,11 +43,12 @@ NITepro <- readxl::read_excel(path=paste0("//dcbcpbsna01a.ENT.dfo-mpo.ca/SCD_Sta
 #                                                              CALCULATE BROODSTOCK AGES 
 
 
-# 1. Age summary for return year of interest 
-    # Return year of interest is assumed to be the max year in the EPRO file (line ~55)
+# 1. Broodstock age summary for return year of interest 
+    # Return year of interest is assumed to be the year indicated in the termNIT mapping file
 
-NITage_summary <- full_join(NITepro %>% 
-                              filter(`(R) RETURN YEAR` %in% NITmap$TermRun_Year & grepl("Nitinat R Fall Chinook", Spawning.Stock) & 
+NIT_broodstock_ages <- full_join(NITepro %>% 
+                              filter(`(R) RETURN YEAR` %in% NITmap02$TermRun_Year & 
+                                       grepl("Nitinat R Fall Chinook", Spawning.Stock) & 
                                        `(R) RESOLVED TOTAL AGE`%in%c(1:6)) %>% 
                               group_by(Maturity.Class, `(R) RESOLVED TOTAL AGE`) %>% 
                               summarize(n=n()) %>%
@@ -55,30 +56,47 @@ NITage_summary <- full_join(NITepro %>%
                             full_age_range) %>%
   complete(Maturity.Class, `(R) RESOLVED TOTAL AGE`, fill=list(`(R) RESOLVED TOTAL AGE`=0), explicit=F) %>%
   group_by(Maturity.Class) %>%
-  mutate(propn = case_when(Maturity.Class %in% c("Male","Female", "Jack") ~ n/sum(n,na.rm=T),
+  mutate(propn = case_when(Maturity.Class %in% c("Male", "Female", "Jack") ~ n/sum(n,na.rm=T),
                            Maturity.Class=="Unsexed Adult" ~ 9999999999999999)) %>%
   mutate(TermRun_AGEStemp = "Broodstock, morts, other",
          TermRun_AGESspat = "Broodstock, morts, other",
          TermRun_AGESsex = case_when(Maturity.Class %in% c("Male", "Female", "Jack") ~ paste0("Broodstock, morts, other - ", Maturity.Class),
                                      Maturity.Class=="Unsexed Adult" ~ "Broodstock, morts, other - Total",
+                                     ## ^^ If there were any "unknown" broodstock, this should be where they are accounted for (as "...Total") ^^
                                      TRUE ~ "FLAG"),
          TermRun_AGES_year = max(NITepro$`(R) RETURN YEAR`)) %>%
-  pivot_wider(names_from=`(R) RESOLVED TOTAL AGE`, values_from=c(n, propn), names_prefix="age ") %>%
+  pivot_wider(names_from=`(R) RESOLVED TOTAL AGE`, values_from=c(n, propn), names_prefix="age_") %>%
   print()
   
 
 
 ############################################################################################################################################################
 
-#                                                            Join mapping file to AGE summary
+#                                                                       Join + Export
 
 
-# JOIN to mapping file
-tt <- left_join(NITmap,
-                NITage_summary,
-                by=c("TermRun_AGEStemp", "TermRun_AGESspat", "TermRun_AGESsex", "TermRun_AGES_year"))
+# ============================== JOIN Broodstock ages to NITmapping file ==============================
+
+NITmap03 <- left_join(NITmap02,
+                      NIT_broodstock_ages,
+                      by=c("TermRun_AGEStemp", "TermRun_AGESspat", "TermRun_AGESsex", "TermRun_AGES_year")) %>%
+  mutate(Maturity.Class = coalesce(Maturity.Class.x, Maturity.Class.y),
+         propn_age_2 = coalesce(propn_age_2.x, propn_age_2.y),
+         propn_age_3 = coalesce(propn_age_3.x, propn_age_3.y),
+         propn_age_4 = coalesce(propn_age_4.x, propn_age_4.y),
+         propn_age_5 = coalesce(propn_age_5.x, propn_age_5.y),
+         propn_age_6 = coalesce(propn_age_6.x, propn_age_6.y),
+         .keep="unused") 
 
 
+# ============================== EXPORT ==============================
+# To github repo ---------------------------
+writexl::write_xlsx(NITmap03, 
+                    path=paste0(here::here("termNIT"), "/", analysis_year, 
+                                "/R_OUT - TERMNIT_mapping_",
+                                analysis_year,
+                                "-output_from_03",
+                                ".xlsx"))
 
 
 

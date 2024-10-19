1b9ecedc7fd72e3cfcc3818ebe3689c10d224218
Katie Davidson <katie.davidson92@gmail.com>
2024-10-18 17:24:49 -0700

    overhaul of pooling in order to get all age-month-year combinations to apply pooled ages across levels

--- termNIT/2023/01-terminal-catch.R
+++ termNIT/2023/01-terminal-catch.R
@@ -3,13 +3,13 @@
 # oct 2024
 
 
-# SET UP 
+# ============================= SET UP ============================= 
+# Load high-use packages -------------------------------
 library(tidyverse)
 
-
-# Helpers
-full_age_range <- tibble(#MONTH=c("June", "July", "August", "September"),
-                              RESOLVED_AGE = c(2:6)) 
+# Helpers -------------------------------
+full_age_range <- tibble(RESOLVED_AGE = c(2:6)) 
+full_month_range <- tibble(MONTH=c("June", "July", "August", "September"))
 "%notin%" <- Negate("%in%")
 options(scipen=9999)
 analysis_year <- 2023
@@ -77,20 +77,42 @@ SCrecBio <- readxl::read_excel(path=paste0("//dcbcpbsna01a.ENT.dfo-mpo.ca/SCD_St
 
 # Joining catch and age data, calculating sample rate -------------------------------
 NITrecCatchbyAge <- full_join(SCrecCatch %>%
-                                filter(SPECIES=="CHINOOK SALMON", DISPOSITION=="Kept", MONTH%in%c("July", "August", "September"), 
+                                filter(SPECIES%in%c("CHINOOK SALMON", "BOAT TRIPS"), DISPOSITION%in%c("Kept", "Effort"), MONTH%in%c("July", "August", "September"), 
                                        CREEL_SUB_AREA %in%c("21A", "21B", "121C", "Area 21")) %>% 
                                 group_by(YEAR) %>% 
-                                mutate(subareas = paste0(unique(CREEL_SUB_AREA), collapse=", ")) %>%
-                                group_by(YEAR, MONTH) %>% 
-                                summarize(monthly_catch_estimate = sum(ESTIMATE, na.rm=T), subareas = unique(subareas)),
+                                mutate(subareas_catch = paste0(unique(CREEL_SUB_AREA), collapse=", ")) %>%
+                                group_by(YEAR, MONTH, SPECIES) %>% 
+                                summarize(monthly_catch_estimate = sum(ESTIMATE, na.rm=T), subareas_catch=unique(subareas_catch)) %>% 
+                                pivot_wider(names_from = SPECIES, values_from = monthly_catch_estimate) %>% 
+                                mutate(`CHINOOK SALMON` = case_when((!is.na(`BOAT TRIPS`) | `BOAT TRIPS`>0) & is.na(`CHINOOK SALMON`) ~ 0,
+                                                                    TRUE ~ `CHINOOK SALMON`)) %>% 
+                                pivot_longer(cols=c(`CHINOOK SALMON`, `BOAT TRIPS`), names_to = "SPECIES", values_to = "monthly_catch_estimate") %>% 
+                                filter(SPECIES=="CHINOOK SALMON") %>%
+                                select(-c(SPECIES)),
                               SCrecBio %>% 
                                 filter(SPECIES=="124", DISPOSITION=="Kept", SUBAREA%in%c("21A", "21B", "121C"), SAMPLE_TYPE=="Sport", !is.na(RESOLVED_AGE)) %>% 
+                                mutate(subareas_sample = paste0(unique(SUBAREA), collapse=", ")) %>%
                                 group_by(YEAR, MONTH, RESOLVED_AGE) %>% 
-                                summarize(n = n()) %>% 
+                                summarize(n = n(), subareas_sample=unique(subareas_sample)) %>% 
+                                full_join(., full_month_range) %>%
+                                full_join(., full_age_range) %>% 
                                 ungroup() %>%
-                                complete(YEAR, MONTH, RESOLVED_AGE, fill=list(n=0)) 
+                                complete(YEAR, MONTH, RESOLVED_AGE, fill=list(n=0)) %>% 
+                                filter(!is.na(RESOLVED_AGE) & !is.na(YEAR)) %>%
+                                group_by(YEAR, MONTH) %>% 
+                                fill(subareas_sample, .direction = "updown") %>% 
+                                mutate(subareas_sample = case_when(is.na(subareas_sample) ~ "no samples",
+                                                                   TRUE ~ subareas_sample))
                               ) %>%
   ungroup() %>% 
+  mutate(subareas = case_when(subareas_sample==subareas_catch ~ subareas_sample,
+                              subareas_sample!=subareas_catch ~ select(., subareas_sample, subareas_catch) %>% reduce(stringr::str_c, sep=", "),
+                              is.na(subareas_sample) | is.na(subareas_catch) ~ coalesce(subareas_sample, subareas_catch))) %>% 
+  select(-c(subareas_catch, subareas_sample)) %>%
+  #complete(YEAR, MONTH, RESOLVED_AGE, fill=list(n=0)) %>%
+  #filter(!is.na(RESOLVED_AGE)) %>% 
+  group_by(YEAR, MONTH) %>%
+  #fill(c(monthly_catch_estimate, subareas_catch), .direction="updown") %>%
   mutate(MONTH = factor(MONTH, levels=month.name)) %>%
   arrange(YEAR, MONTH, RESOLVED_AGE) %>% 
   ungroup() %>%
@@ -99,9 +121,10 @@ NITrecCatchbyAge <- full_join(SCrecCatch %>%
   mutate(n = case_when(is.na(n) ~ 0,
                        TRUE ~ n),
          month_sample_size = sum(n, na.rm=T),
-         propn = n/month_sample_size,
+         propn = case_when(month_sample_size==0 ~ 0,
+                           TRUE ~ n/month_sample_size),
          biosample_rate = case_when(MONTH=="June" ~ NA,
-                                    MONTH %notin% "June" & monthly_catch_estimate==0 ~ 0,
+                                    MONTH %notin% "June" & (monthly_catch_estimate==0 | month_sample_size==0) ~ 0,
                                     TRUE ~ month_sample_size/monthly_catch_estimate)) %>%
   print()
 
@@ -116,7 +139,7 @@ pdf(file = here::here("termNIT", "2023", "figures",
 
 ggplot() +
   geom_bar(data=NITrecCatchbyAge %>% 
-             filter(!is.na(RESOLVED_AGE)),
+             filter(propn>0),
            aes(x=plyr::mapvalues(MONTH, from=month.name, to=month.abb), y=propn, 
                fill=as.factor(RESOLVED_AGE), colour=as.factor(RESOLVED_AGE)), stat="identity", alpha=0.8) +
   geom_text(data=NITrecCatchbyAge %>% 
@@ -162,7 +185,6 @@ ggplot(data = NITrecCatchbyAge,
   geom_hline(yintercept=0.1, colour="gray60", linetype="dashed", size=1) +
   geom_point(size=3, alpha=0.5) +
   geomtextpath::geom_textline(linewidth=1, show.legend=F, hjust=0.38, text_smoothing=30, size=4, fontface=2, alpha=0.8) +
-  #geom_text(aes(label=month_sample_size)) +
   scale_y_continuous(labels = scales::percent) +
   labs(y="Sampling rate", x="", fill="Biosampling rate over \nmost recent 10 years:", colour="Biosampling rate over \nmost recent 10 years:") +
   theme_bw() +
@@ -193,28 +215,79 @@ NITrecCatchbyAge_pooled <- NITrecCatchbyAge %>%
   ) %>% 
   group_by(YEAR, temporal_pool_it1) %>% 
   # Calculate new pooled sample size resulting from pooling iteration 1: 
-  mutate(pool_sample_size = sum(n, na.rm=T)) %>% 
+  mutate(pool_sample_size1 = sum(n, na.rm=T)) %>% 
   # Calculate the new total catch estimate for the pooling results from iteration 1, starting with pasting in July's catch alone (don't want June catch):
-  mutate(pool_catch_estimate = case_when(MONTH=="July" ~ monthly_catch_estimate)) %>% 
+  mutate(pool_catch_estimate1 = case_when(MONTH=="July" ~ monthly_catch_estimate)) %>% 
   group_by(YEAR, temporal_pool_it1) %>% 
   # Now insert the pooled catch estimate for cases where Aug and Sept were pooled:
-  mutate(pool_catch_estimate = case_when(MONTH%in%c("August", "September") ~ sum(unique(monthly_catch_estimate),na.rm=T),
-                                         TRUE ~ pool_catch_estimate),
+  mutate(pool_catch_estimate1 = case_when(MONTH%in%c("August", "September") ~ sum(unique(monthly_catch_estimate),na.rm=T),
+                                         TRUE ~ pool_catch_estimate1),
          # Calculate the new sample rate after pooling iteration 1: 
-         pool_sample_rate = case_when(pool_catch_estimate==0 ~ NA,
-                                      TRUE ~ pool_sample_size/pool_catch_estimate)) %>% 
+         pool_sample_rate1 = case_when(pool_catch_estimate1==0 ~ NA,
+                                      TRUE ~ pool_sample_size1/pool_catch_estimate1)) %>% 
   group_by(YEAR) %>%
   # --- POOL ITERATION 2: If, after iteration 1, sample rates are still < 10%, pool all months together for the entire year 
   mutate(temporal_pool_it2 = case_when((any(temporal_pool_it1=="June, July") & any(temporal_pool_it1=="August, September")) & 
-                                          (pool_sample_rate < 0.1 | pool_sample_size<5 | is.na(pool_sample_rate)) ~
+                                          (pool_sample_rate1 < 0.1 | pool_sample_size1<5 | is.na(pool_sample_rate1)) ~
                                          paste(unique(temporal_pool_it1), collapse = ", "),
                                        TRUE ~ temporal_pool_it1)) %>% 
   ungroup() %>%
+  # --- FINAL AGE COMP
+  # Calculate number of age samples per year/pooling category (i.e., pooled numerator): 
+  group_by(YEAR, temporal_pool_it2, RESOLVED_AGE) %>% 
+  mutate(pool_n = sum(n, na.rm=T)) %>% 
+  # Roll up further to calculate the total sample size for each year/pool category (i.e., pooled denominator), and then calculate final rolled-up age comp: 
+  group_by(YEAR, temporal_pool_it2) %>% 
+  mutate(pool_sample_size2 = sum(unique(pool_n)),
+         pool_propn = case_when(pool_sample_size2==0 ~ 0,
+                           TRUE ~ pool_n/pool_sample_size2)) %>%
   print()
 
 
 
 
+
+
+
+tt<-NITrecCatchbyAge_pooled %>% 
+  mutate(MONTH = factor(MONTH, levels=month.name)) %>%
+  arrange(YEAR, MONTH, RESOLVED_AGE) %>%
+  # # Roll up sample sizes etc for each age/month within the year-pooling categories: 
+  # group_by(YEAR, temporal_pool_it2, RESOLVED_AGE, MONTH) %>% 
+  # summarize(n_samples=sum(n, na.rm=T), monthly_catch_estimate=unique(monthly_catch_estimate),  
+  #           month_sample_size=unique(month_sample_size), subareas=unique(subareas)
+  #           ) %>% 
+
+  
+  # Then roll it up further by calculating the # of age samples per year/pooling category (i.e., pooled numerator):
+  #group_by(YEAR, temporal_pool_it2, RESOLVED_AGE) %>% 
+  #summarize(n = sum(n_samples, na.rm=T), MONTH=unique(MONTH)) %>%
+  # Roll up further to calculate the total sample size for each year/pool category (i.e., pooled denominator), and then calculate final rolled-up age comp: 
+  # group_by(YEAR, temporal_pool_it2) %>%
+  # mutate(pool_sample_size2 = sum(unique(n), na.rm=T),
+  #        propn = case_when(pool_sample_size2==0 ~ 0,
+  #                          TRUE ~ n/pool_sample_size2)) %>% 
+
+  # Sort by age so that when pivoted wide-form, the columns are in order of age smallest-largest left-right:
+  arrange(RESOLVED_AGE) %>%
+  # Pivot wide-form: 
+  pivot_wider(names_from = RESOLVED_AGE, values_from = c(n, propn), names_prefix = "age_") %>%
+  
+  
+  distinct()
+  select(-c(contains("_NA"))) %>% 
+  arrange(YEAR) %>%
+  # If the year of interest is present in the data series, then retain that year only; otherwise, select last year's data:
+  filter(if_else(YEAR%in%NITmap$TermRun_Year, TRUE,  YEAR==(YEAR-1))) %>%
+  # Rename/create columns to assist with joining
+  mutate(TermRun_sector01 = "Recreational",
+         TermRun_sector02 = "Area 21 Terminal") %>% 
+  rename(Enumeration = monthly_catch_estimate,
+         TermRun_AGEStemp = temporal_pool_it2,
+         TermRun_temp_strata = MONTH,
+         TermRun_AGESspat = subareas,
+         TermRun_AGES_year = YEAR) %>% 
+  mutate(across(everything(), as.character))
 
 
 
@@ -228,17 +301,14 @@ NITmap01 <- left_join(NITmap %>%
                         mutate(across(everything(), as.character)),
                       
                       NITrecCatchbyAge_pooled %>% 
-                        # Calculate new pooled age composition: 
-                        group_by(YEAR, temporal_pool_it2, RESOLVED_AGE) %>% 
-                        summarize(n = sum(n, na.rm=T), monthly_catch_estimate=unique(monthly_catch_estimate), MONTH=unique(MONTH), 
-                                  month_sample_size=unique(month_sample_size), subareas=subareas) %>% 
-                        group_by(YEAR, temporal_pool_it2) %>% 
-                        mutate(sample_size = sum(unique(n), na.rm=T),
-                               propn = case_when(sample_size==0 ~ 0,
-                                                 TRUE ~ n/sample_size)) %>% 
-                        arrange(RESOLVED_AGE) %>%
-                        pivot_wider(names_from = RESOLVED_AGE, values_from = c(n, propn), names_prefix = "age_") %>%
-                        select(-c(contains("_NA"))) %>% 
+                        group_by(YEAR, temporal_pool_it2, RESOLVED_AGE, MONTH) %>% 
+                        summarize(monthly_catch_estimate = unique(monthly_catch_estimate),
+                                  original_n = unique(month_sample_size),
+                                  n = unique(pool_n),
+                                  propn = unique(pool_propn),
+                                  subareas = unique(subareas)
+                                  ) %>% 
+                        pivot_wider(names_from = RESOLVED_AGE, values_from = c(n, propn)) %>% 
                         arrange(YEAR) %>%
                         # If the year of interest is present in the data series, then retain that year only; otherwise, select last year's data:
                         filter(if_else(YEAR%in%NITmap$TermRun_Year, TRUE,  YEAR==(YEAR-1))) %>%
@@ -266,7 +336,16 @@ NITmap01 <- left_join(NITmap %>%
   
 
 
-  
+
+# ============================== EXPORT ==============================
+# To github repo ---------------------------
+writexl::write_xlsx(NITmap01, 
+                    path=paste0(here::here("termNIT"), "/", analysis_year, 
+                                "/R_OUT - TERMNIT_mapping_",
+                                analysis_year,
+                                "-output_from_01",
+                                ".xlsx"))
+
 
 
 

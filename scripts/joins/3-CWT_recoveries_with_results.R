7e01f718a6cfff066b45d51b5d72c639b9461ae2
Katie Davidson <katie.davidson92@gmail.com>
2025-01-10 11:29:07 -0800

    push updates

--- scripts/joins/3-CWT_recoveries_with_results.R
+++ scripts/joins/3-CWT_recoveries_with_results.R
@@ -48,19 +48,19 @@ library(tidyverse)
 
 ################################################################################################################################################
 
-#                                                                           I. Compile individual year files
+#                                                                           I. Compile/load recoveries 
 
 
-# Option 1:   Run compile file (annual or semi-annual, not required every time) ------------------
+# Option 1:   Run compile file to compile year-specific files (annual or semi-annual, not required every time *SLOW*) ------------------
 # source(here::here("scripts", "misc-helpers", "HeadRcvyCompile.R"))
-  # saves as mrpHeadRcvy
+  # saves as CN_headRcvy
 
 
 # Option 2:   Load already compiled file from above ------------------
 # there is an excel file available, but it often times out memory allocation, so recommend loading the csv version 
-mrpHeadRcvy <- read.csv(file=list.files(path = "//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/CHINOOK/WCVI_TERMINAL_RUN/Annual_data_summaries_for_RunRecons/HeadRcvyCompile_base-files/2-Export-from-R",
+CN_headRcvy <- read.csv(file=list.files(path = "//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/CHINOOK/WCVI_TERMINAL_RUN/Annual_data_summaries_for_RunRecons/HeadRcvyCompile_base-files/2-Export-from-R",
                                         pattern = "^R_OUT - MRPHeadRecoveries_CHINOOK_.*\\.csv$",    
-                                        full.names = TRUE))
+                                        full.names = TRUE)) 
                    
 
 # Load file as a tibble
@@ -73,54 +73,87 @@ mrpHeadRcvy <- read.csv(file=list.files(path = "//ENT.dfo-mpo.ca/DFO-MPO/GROUP/P
 
 ################################################################################################################################################
 
-#                                                                           II. CWT Releases Load
+#                                                                           II. Compile/load releases
 
 
-# Load files from local repo -------------
+# Option 1:   Run saaWeb query to dump releases from MRP (annual or semi-annual, not required every time, *VERY SLOW*) ------------------
+# source(here::here("scripts", "functions", "pullChinookCWTReleases.R"))
+# saves as cn_relTagCodes
 
 
-# Load file as a tibble
-cwt_rel <- list.files(here("outputs"), pattern = "(?i)release.*tagcodes", full.names = T) %>%
-  str_subset("(?i)chinook") %>% # The Chinook data (as opposed to all species)
-  read_excel |> 
-  rename_with(~str_replace_all(.x, "MRP", "MRP_REL")) # Clarify which columns come from the release data
+# Option 2:   Load already compiled file from above ------------------
+# there is an excel file available, but it often times out memory allocation, so recommend loading the csv version 
+CN_relTagCodes <- readxl::read_excel(path=list.files(path = "//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/CHINOOK/WCVI_TERMINAL_RUN/Annual_data_summaries_for_RunRecons",
+                                                     pattern = "^R_OUT - Chinook CWT release tagcodes BY .*\\.xlsx$",    
+                                                     full.names = T),
+                                     sheet="Sheet1")
 
 
 
-#############################################################################################################################################################
-
-#                                                                           III. Join releases to recoveries
-
-
-# Join EPRO master file to NPAFC master mark file ---------------------------
-intersect(colnames(head_rec), colnames(cwt_rel))
-
-wcviCNheads_w_cwt <- left_join(head_rec,
-                               cwt_rel,
-                               by=c("MRP_TagCode" = "MRP_REL_Tagcode"),
-                               relationship="many-to-one") |> 
-  select(-`MRP_REL_Species Name`, -`MRP_REL_Recovery Years`) # Remove some columns from the releases that are redundant with the recovery information
+# # Load file as a tibble
+# cwt_rel <- list.files(here("outputs"), pattern = "(?i)release.*tagcodes", full.names = T) %>%
+#   str_subset("(?i)chinook") %>% # The Chinook data (as opposed to all species)
+#   read_excel |> 
+#   rename_with(~str_replace_all(.x, "MRP", "MRP_REL")) # Clarify which columns come from the release data
 
 
 
 #############################################################################################################################################################
 
-#                                                                           IV. Save the output
+#                                                                           III. Join recovery records to release tag codes for Stock ID
 
 
-# Export joined data file to .xslx --------------------------- 
+# Join head recoveries to tag codes for release stock IDs ---------------------------
+CN_headRcvyResults <- left_join(CN_headRcvy,
+                                CN_relTagCodes,
+                               by=c("MRP_TagCode" = "(R) TAGCODE"),
+                               relationship="many-to-one") %>% 
+  rename(`(R) SAMPLE YEAR`=X.R..SAMPLE.YEAR,
+         `(R) HEAD LABEL`=X.R..HEAD.LABEL)#|> 
+  #select(-`MRP_REL_Species Name`, -`MRP_REL_Recovery Years`) # Remove some columns from the releases that are redundant with the recovery information
 
-write_xlsx(
-  wcviCNheads_w_cwt,
-  path = here(
-    "outputs", 
-    paste0(
-      "R_OUT - MRPHeadRecoveries_CHINOOK_WITH RESULTS_2012-2023_LastUpdate_",
-      Sys.Date(),
-      ".xlsx"
-    )
-  )
-)
+
+
+#############################################################################################################################################################
+
+#                                                                           X. EXPORT 
+
+
+# ================== Export ================== 
+
+# To github repo ---------------------------
+writexl::write_xlsx(CN_headRcvyResults, 
+                    path=paste0(here::here("outputs"), 
+                                "/R_OUT - MRPHeadRecoveries_CHINOOK_WITH RESULTS_",
+                                min(CN_headRcvyResults$`(R) SAMPLE YEAR`),
+                                "-",
+                                max(CN_headRcvyResults$`(R) SAMPLE YEAR`),
+                                ".xlsx"))
+
+
+# To DFO Network drive ---------------------------
+writexl::write_xlsx(CN_headRcvyResults, 
+                    path=paste0("//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/WCVI/CHINOOK/WCVI_TERMINAL_RUN/Annual_data_summaries_for_RunRecons/HeadRcvyCompile_base-files/2-Export-from-R",
+                                "/R_OUT - MRPHeadRecoveries_CHINOOK_WITH RESULTS_",
+                                min(CN_headRcvyResults$`(R) SAMPLE YEAR`),
+                                "-",
+                                max(CN_headRcvyResults$`(R) SAMPLE YEAR`),
+                                ".xlsx"))
+
+
+# # Export joined data file to .xslx --------------------------- 
+# 
+# write_xlsx(
+#   wcviCNheads_w_cwt,
+#   path = here(
+#     "outputs", 
+#     paste0(
+#       "R_OUT - MRPHeadRecoveries_CHINOOK_WITH RESULTS_2012-2023_LastUpdate_",
+#       Sys.Date(),
+#       ".xlsx"
+#     )
+#   )
+# )
 
 
 # /END!

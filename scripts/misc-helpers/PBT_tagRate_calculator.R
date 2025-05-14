792a460a1db5967becb734bfd7d49bdf3ac55bd3
Katie Davidson <katie.davidson92@gmail.com>
2025-05-14 15:28:30 -0700

    updated PBT inventory data call and exported tag rates

--- scripts/misc-helpers/PBT_tagRate_calculator.R
+++ scripts/misc-helpers/PBT_tagRate_calculator.R
@@ -1,6 +1,8 @@
 # PBT summary pivot
+library(tidyverse)
 
 
+# Read in PBT inventory data --------------
 pbtsummary <- readxl::read_excel(path="//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Operations/SCA/SCD_Stad/SC_BioData_Management/15-DNA_Results/PBT/Chinook/results/2024-12-05 Chinook_PBT Summary_2013-2023.xlsx") %>%
   filter(`wcvi y/n`=="WCVI") %>%
   group_by(Species, `wcvi y/n`, Brood_Regional_Area, Brood_Year, Brood, Brood_Recorded, Brood_Genotyped) %>%
@@ -8,8 +10,19 @@ pbtsummary <- readxl::read_excel(path="//ENT.dfo-mpo.ca/DFO-MPO/GROUP/PAC/PBS/Op
             Brood_Genotyped = unique(Brood_Genotyped), 
             BY_tagrate = case_when(Brood_Recorded==0 ~ NA,
                                    TRUE ~ Brood_Genotyped/Brood_Recorded)) %>%
-  arrange(Brood, Brood_Year)  
+  arrange(Brood, Brood_Year) 
 
 
-  pivot_wider(names_from = Brood,
-              values_from = Stock_Tag_Rate)
+
+
+# ================== Export ================== 
+# To github repo ---------------------------
+writexl::write_xlsx(pbtsummary,
+                    path=here::here("outputs", "R_OUT - PBT_Tag_Rates_AllStocks-All BYs.xlsx"))
+
+
+
+# To DFO Network drive ---------------------------
+writexl::write_xlsx(pbtsummary,
+                    path=paste0("//dcbcpbsna01a.ENT.dfo-mpo.ca/PBS_SA_DFS$/SCD_Stad/SC_BioData_Management/15-DNA_Results/PBT/Chinook/",
+                                "R_OUT - PBT_Tag_Rates_AllStocks-All BYs.xlsx"))
